0d258479c6845d96337bf046625188630a1828a8
yitian <yitian.z@foxmail.com>
2019-10-16 11:32:01 +0800

    update rescheduling yaml config

--- load-aware-scheduler/src/main/java/zyt/custom/scheduler/aurora/AuroraSchedulerController.java
+++ load-aware-scheduler/src/main/java/zyt/custom/scheduler/aurora/AuroraSchedulerController.java
@@ -10,24 +10,36 @@ import java.lang.reflect.Method;
 import java.util.logging.Logger;
 
 /**
- * Algorithm implementation for HOT-EDGE and LOAD-AWARE
+ * This class extends ReschedulerController that has only one function named "rescheduling".
+ * In the rescheduling, it invokes the "reschedule" function defined in AuroraScheduler interface.
+ *
+ * AuroraScheduler is a interface that declare a function named "reschedule".
+ * And, ExampleRescheduler, DataStreamCateRescheduler and LoadAwareRescheduler implemented AuroraScheduler.
+ * They defined the specific function content of the reschedule to implement your own rescheduling algorithm.
+ *
+ * AuroraSchedulerController, ReschedulerController, AuroraRescheduler and ExampleRescheduler (DataStreamCate/LoadAware)
+ * build the Bridge Pattern to implement the flexible structure of Heron rescheduling.
+ *
+ *
+ *
+ * Processing of this file:
+ * 2018-07-03:
+ *      add for re-schedule topology.
+ * 2018-07-06:
+ *      running not successfully for now, detailing exception at POST about this.
+ * 2018-07-18:
+ *      sovled reschedule core problem
+ * 2019-10-15:
+ *      restructured this class
  *
  * @author yitian
- *
- * 2018-07-03 add for re-schedule topology
- * 2018-07-06 noted:
- *      is not successfully for now
- *      detailing exception at POST about this
- * 2018-07-18 noted:
- *      sovled reschedule core problem
- *
- * TODO: reconstructe this class
  */
 public class AuroraSchedulerController extends ReschedulerController {
 
-    private static final Logger LOG = Logger.getLogger(AuroraSchedulerController.class.getName());
+    private static final Logger logger= Logger.getLogger(AuroraSchedulerController.class.getName());
 
     private Config config;
+
     private Config runtime;
 
     private AuroraRescheduler auroraRescheduler;
@@ -53,18 +65,21 @@ public class AuroraSchedulerController extends ReschedulerController {
     }
 
     /**
+     * Reading the full path of a rescheduler class configured in "online-rescheduling.yaml".
+     * Build the instance and invoke the "initialize" function to init variables of the class.
      * TODO: restructure modified 4
      * @return
      */
     public Object loadRescheduler() {
-        String className = (String) YamlUtils.getInstance().getValueByKey("rescheduling", "algorithm0");
+        String className = (String) YamlUtils.getInstance().getValueByKey("rescheduling", "active");
         Class clazz = null;
         try {
             // create instance of AuroraRescheduler
             clazz = Class.forName(className);
-            Object obj = clazz.newInstance();
+            logger.info("Loaded the rescheduler class is: " + clazz.getName());
 
             // invoke the initialize function
+            Object obj = clazz.newInstance();
             Method method = clazz.getMethod("initialize", Config.class, Config.class);
             method.invoke(obj, config, runtime);
             return obj;
